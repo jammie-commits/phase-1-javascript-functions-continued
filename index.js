16e7e604433fe4089aa8049eab532f2247ac0ce9
jammie-commits <jay.mbugua.ph@gmail.com>
2024-03-23 16:49:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  function wrapAdjective(visualFlair = "*") {
+    return function (adjective = "special") {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  
+  console.log(saturdayFun());  
+  console.log(saturdayFun("play board games"));  
+  
+  console.log(mondayWork());  
+  console.log(mondayWork("work from home"));  
+  
+  console.log(encouragingPromptFunction("a dedicated programmer")); 
+  console.log(wrapAdjective("%")("amazing"));  
+   
+      
